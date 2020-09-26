e4deb00973a3544d89d8690489af9cac2836eedc
Tobias Hölzer <tobiashoelzer@hotmail.com>
2020-09-26 16:39:42 +0200

    Frameworks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Frameowrks from './Frameworks';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React</h1>
+        <p>A JavaScript library for building user interfaces</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <Frameowrks></Frameowrks>
+      </main>
     </div>
   );
 }
---
+++ src/Frameworks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Frameworks() {
+  return (
+    <div className="Frameworks">
+      Hello
+    </div>
+  );
+}
+
+export default Frameworks;

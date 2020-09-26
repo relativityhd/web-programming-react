d8ccb718b35432c0a9ed9da4819eaf18f5df4412
Philipp Wussow <wi19235@lehre.dhbw-stuttgart.de>
2020-09-26 16:59:04 +0200

    Reactsite

---
+++ src/reactsite.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function Reactsite() {
+  return (
+    <div className="SiteWrapper">
+      <div className="reactsite">
+        <h1>Intro to React</h1>
+        <ul>
+            <li>Deklaratives Web-frontend Framework</li>
+            <li>Komponentenbasiert mit einem State und einem Lifecycle</li>
+            <li>Entwickelt von Jordan Walke in 2011</li>
+            <li>Weiterentwickelt von Facebook OpenSource</li>
+            <li>Benutzt von Facebook, Instagram, Discord und vielen mehr</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Reactsite;
